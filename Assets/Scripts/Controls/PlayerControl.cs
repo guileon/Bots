5fb3f642648521b28822adef9601042d660a2482
guileon <guileon1234@gmail.com>
2016-01-05 02:54:52 -0200

    add player control script

---
+++ Assets/Scripts/Controls/PlayerControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+	private Transform t;
+	private Rigidbody rb;
+	private float x;
+	private float y;
+	void Start () {
+		x = 0;
+		y = 0;
+		t = GetComponent<Transform> ();
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		x = Input.GetAxis("Horizontal");
+		y = Input.GetAxis("Vertical");
+		t.Translate (new Vector3 (x, 0, y));
+
+	}
+}
