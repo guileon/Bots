ac94c4144562b535dbf44deb0682447c6d9e1fb8
guileon <guileon1234@gmail.com>
2016-01-05 02:54:06 -0200

    refactor code

--- Assets/Scripts/LevelGeneration/MapReader.cs
+++ Assets/Scripts/LevelGeneration/MapReader.cs
@@ -3,38 +3,41 @@ using System.Collections;
 using TiledSharp;
 using System.Collections.Generic;
 public class MapReader : MonoBehaviour {
+	public TmxMap map = new TmxMap("Assets/Resources/Maps/sample.tmx");
 
 	void Start () {
-		Dictionary<int, string> terrainDict = new Dictionary<int, string>();
-		var map = new TmxMap("Assets/Resources/Maps/sample.tmx");
+		var terrainDict = createTerrainDict(map.Tilesets);
+
 		TmxList<TmxLayer> layers = map.Layers;
-
-		//Creating the terrain dictionary
-		int tilesetNumber = 0;
-		var terrains = map.Tilesets[tilesetNumber].Terrains;
-		int firstGid = map.Tilesets [tilesetNumber].FirstGid;
-		foreach (var t in terrains) {
-			terrainDict.Add (t.Tile+firstGid, t.Name);
-		}
-		foreach (KeyValuePair<int, string> bla in terrainDict)
-			Debug.Log (bla.Key.ToString() + " " + bla.Value);
-
-
-		//Placing the prefabs in the world
 		foreach (var layer in map.Layers) {
-			foreach (var tile in layer.Tiles) {
-				string name;
-				if (terrainDict.TryGetValue (tile.Gid, out name)) {
-					Object test = Resources.Load ("Prefabs/" + name);
-					Instantiate (test, new Vector3 (tile.X, 0.5f, tile.Y), Quaternion.identity);
-				} else
-					Debug.Log (tile.Gid);
+			drawLayer (layer, terrainDict);
+		}
+	}
+
+	/* creates dictionary for tiles */
+	private Dictionary<int, string> createTerrainDict(TmxList<TmxTileset> sets) {
+		Dictionary<int, string> tdict = new Dictionary<int, string>();
+
+		foreach (var set in sets) {
+			var firstGid = set.FirstGid;
+			foreach (var terrain in set.Terrains) {
+				tdict.Add (terrain.Tile + firstGid, terrain.Name);
 			}
 		}
 
+		return tdict;
+	}
 
-
-
+	/* draws layers */
+	private void drawLayer(TmxLayer layer, Dictionary<int, string> terrainDict) {
+		foreach (var tile in layer.Tiles) {
+			string name;
+			if (terrainDict.TryGetValue (tile.Gid, out name)) {
+				Object test = Resources.Load ("Prefabs/" + name);
+				Instantiate (test, new Vector3 (tile.X, 0.5f, tile.Y), Quaternion.identity);
+			} else
+				Debug.Log (tile.Gid);
+		}
 	}
 	
 	// Update is called once per frame
