ee361ce71a25e5eca9e3d80f2e6b6697bd47c6b0
guileon <guileon1234@gmail.com>
2016-01-07 13:07:22 -0200

    Added object reading. Started controlling the player. Failed.

--- Assets/Scripts/Controls/PlayerControl.cs
+++ Assets/Scripts/Controls/PlayerControl.cs
@@ -1,22 +1,100 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class PlayerControl : MonoBehaviour {
-	private Transform t;
 	private Rigidbody rb;
 	private float x;
 	private float y;
+	Vector3 targetPosition;
+	bool semaforol;
+	enum movements {up, down, left, right, none};
+	Queue<movements> movementQueue;
+
+	Transform target;
+
 	void Start () {
+		semaforol = false;
+		movementQueue = new Queue<movements> ();
 		x = 0;
 		y = 0;
-		t = GetComponent<Transform> ();
-		rb = GetComponent<Rigidbody>();
+		rb =GetComponentInChildren<Rigidbody>();
+		targetPosition = rb.position;
 	}
 
 	void Update () {
-		x = Input.GetAxis("Horizontal");
-		y = Input.GetAxis("Vertical");
-		t.Translate (new Vector3 (x, 0, y));
+//		rb.AddForce (new Vector3 (1, 0, 0));
+//
+//		Object g = Resources.Load ("Prefabs/ghost");
+//		Object h = Instantiate(g,positi
+//
+//		if (Physics.CheckSphere (new Vector3(this.transform.position.x, 0, this.transform.position.z) + new Vector3 (3, 0, 0), .2f)) {
+//			Debug.Log ("CaCAroto");
+//		} else
+//			Debug.Log ("Kurumin");
 
+//		rb.rotation = Quaternion.identity;
+//
+//		Vector3 movementDirection = new Vector3 (0, 0, 0);
+//		Vector3 currentPosition = rb.position;
+//		const float speed = 1;
+
+		float step = 0.3f;
+
+		var go = getNextMovement () + transform.position;
+
+		Vector3 velocity = Vector3.zero;
+		Debug.Log (go);
+		rb.position = Vector3.SmoothDamp (transform.position, go, ref velocity, step);
+//		if (nextMovement != movements.none) {
+//			movementQueue.Enqueue (nextMovement);
+//			semaforol = true;
+//			currentPosition = rb.position;
+//		}
+//
+//		if (Vector3.Magnitude (rb.position - targetPosition) > .01f) {
+//			Vector3 velocity = rb.velocity;
+//			rb.position = Vector3.SmoothDamp (currentPosition, targetPosition, ref velocity, .001f);
+//			rb.velocity = velocity;
+//		}
+
+		/*semaforol = movementList.Count > 0 ? true : false;
+
+
+		if (semaforol) {
+			if (Mathf.Abs (y) < .1f && Mathf.Abs (x) > .1f) {
+
+				movementDirection = new Vector3 (Mathf.Sign (x) * speed, 0, 0);
+				x = 0;
+			}
+			if (Mathf.Abs (x) < .1f && Mathf.Abs (y) > .1f) {
+				
+				movementDirection = new Vector3 (0, 0, Mathf.Sign (y) * speed);
+				y = 0;
+			}
+
+			currentPosition = rb.position;
+			targetPosition = currentPosition + movementDirection;
+
+			semaforol = false;
+		}
+		Vector3 velocity = rb.velocity;
+		rb.position = Vector3.SmoothDamp (currentPosition, targetPosition, ref velocity, .001f);
+		rb.velocity = velocity;*/
+
+
+	}
+
+	private Vector3 getNextMovement(){
+		if (Input.GetKeyDown (KeyCode.UpArrow))
+			return(new Vector3(1,0,0));
+		else if (Input.GetKeyDown (KeyCode.DownArrow))
+			return(new Vector3(-1,0,0));
+		else if (Input.GetKeyDown (KeyCode.LeftArrow))
+			return(new Vector3(0,0,-1));
+		else if (Input.GetKeyDown (KeyCode.RightArrow))
+			return(new Vector3(0,0,1));
+		else
+			return(new Vector3(0,0,0));
 	}
 }
--- Assets/Scripts/LevelGeneration/MapReader.cs
+++ Assets/Scripts/LevelGeneration/MapReader.cs
@@ -12,8 +12,25 @@ public class MapReader : MonoBehaviour {
 		foreach (var layer in map.Layers) {
 			drawLayer (layer, terrainDict);
 		}
+
+		foreach (var objType in map.ObjectGroups)
+			foreach (var obj in objType.Objects) {
+				Debug.Log (obj.Name);
+				Debug.Log (obj.X/obj.Height);
+				Debug.Log (obj.Y/obj.Width);
+
+				createObject (obj.Name, obj.X, obj.Y, obj.Width, obj.Height);
+
+				if (obj.Name == "start") {
+					createObject("char", obj.X, obj.Y, obj.Width, obj.Height);
+				}
+			}
 	}
 
+	private void createObject(string name, double x, double y, double width, double height)	{
+		Object test = Resources.Load ("Prefabs/" + name);
+		Instantiate (test, new Vector3 ((float)(x/width), 0.0f, (float)(y/height)), Quaternion.identity);
+	}
 	/* creates dictionary for tiles */
 	private Dictionary<int, string> createTerrainDict(TmxList<TmxTileset> sets) {
 		Dictionary<int, string> tdict = new Dictionary<int, string>();
@@ -34,7 +51,8 @@ public class MapReader : MonoBehaviour {
 			string name;
 			if (terrainDict.TryGetValue (tile.Gid, out name)) {
 				Object test = Resources.Load ("Prefabs/" + name);
-				Instantiate (test, new Vector3 (tile.X, 0.5f, tile.Y), Quaternion.identity);
+
+				Instantiate (test, new Vector3 (tile.X, 0.0f, tile.Y), Quaternion.identity);
 			} else
 				Debug.Log (tile.Gid);
 		}
